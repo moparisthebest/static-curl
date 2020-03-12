4bce3c6ad802025b31d9ad75681e3f9f853c3d82
moparisthebest <admin@moparisthebest.com>
2020-03-11 22:33:36 -0400

    curl 7.69.0

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 # docker run --rm -v $(pwd):/tmp i386/alpine /tmp/build.sh
 # docker run --rm -v $(pwd):/tmp ALPINE_IMAGE_HERE /tmp/build.sh
 
-CURL_VERSION='7.69.0'
+CURL_VERSION='7.69.1'
 
 [ "$1" != ""] && CURL_VERSION="$1"
 
